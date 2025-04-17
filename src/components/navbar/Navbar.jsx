d29fbede468edd0cb429a03911a71258eb3dda89
Bernardthe3rd <bammeijer@outlook.com>
2025-04-17 16:29:52 +0200

    formed navbar version one

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import { LinuxLogo } from "@phosphor-icons/react";
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
+        <nav className="container">
+            <ul className="nav__big">
                 <li>Home</li>
                 <li>About</li>
-                <LinuxLogo size={24} />
+                <LinuxLogo size={64} />
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
