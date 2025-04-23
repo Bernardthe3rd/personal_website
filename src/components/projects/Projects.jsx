ad33967b06c13f083a51266e584fbde6e894a47c
Bernardthe3rd <bammeijer@outlook.com>
2025-04-23 16:41:14 +0200

    implemented an alternative text so I can add real projects later

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,38 +1,60 @@
 import './Projects.css'
 import myself from "../../assets/about-myself-img.jpeg";
-import {Element} from "react-scroll";
+import {Element, Link} from "react-scroll";
+import {useState} from "react";
+import {ArrowBendRightDown} from "@phosphor-icons/react";
 
 
 const Projects = () => {
+
+    const [projects, toggleProjects] = useState(true)
+
+    const handleProjects = () => {
+        toggleProjects(!projects)
+    }
+
     return (
-        <Element name="projects" className="container">
-            <div className="element__projects">
-                <article className="element__projects--article">
-                <span className="img-wrapper__projects">
-                    <img src={myself} alt="img of first project" />
-                </span>
-                    <p>
-                        informatie over het project <a href="https://github.com" target="_blank" >github link</a>
-                    </p>
-                </article>
-                <article className="element__projects--article">
-                <span className="img-wrapper__projects">
-                    <img src={myself} alt="img of first project" />
-                </span>
-                    <p>
-                        informatie over het project <a href="https://github.com" target="_blank" >github link</a>
-                    </p>
-                </article>
-                <article className="element__projects--article">
-                <span className="img-wrapper__projects">
-                    <img src={myself} alt="img of first project" />
-                </span>
-                    <p>
-                        informatie over het project <a href="https://github.com" target="_blank" >github link</a>
-                    </p>
-                </article>
-            </div>
-        </Element>
+        projects
+            ?
+            <Element name="projects" className="container">
+                <div className="element__projects-alt">
+                    <h2>PROJECTS</h2>
+                    <p>Sorry no projects yet! But please let me know if you have some inspiration</p>
+                    <Link to="contact" smooth={true} duration={500}>
+                        <ArrowBendRightDown size={64} />
+                    </Link>
+                </div>
+            </Element>
+            :
+            <Element name="projects" className="container">
+                <div className="element__projects">
+                    <h2>PROJECTS</h2>
+                    <article className="element__projects--article">
+                        <span className="img-wrapper__projects">
+                            <img src={myself} alt="img of first project"/>
+                        </span>
+                        <p>
+                            informatie over het project <a href="https://github.com" target="_blank">github link</a>
+                        </p>
+                    </article>
+                    <article className="element__projects--article">
+                        <span className="img-wrapper__projects">
+                            <img src={myself} alt="img of first project"/>
+                        </span>
+                        <p>
+                            informatie over het project <a href="https://github.com" target="_blank">github link</a>
+                        </p>
+                    </article>
+                    <article className="element__projects--article">
+                        <span className="img-wrapper__projects">
+                            <img src={myself} alt="img of first project"/>
+                        </span>
+                        <p>
+                            informatie over het project <a href="https://github.com" target="_blank">github link</a>
+                        </p>
+                    </article>
+                </div>
+            </Element>
     );
 }
 
